ec09b672428e01ec75d070d71eb9d5a4d1d4ccb5
Ashley McCallum <w0765596@myscc.ca>
2021-10-30 22:11:23 -0400

    #16 Add basic content to survey page

--- app/src/main/java/com/ashleymccallum/thebarkinglot/SurveyFragment.java
+++ app/src/main/java/com/ashleymccallum/thebarkinglot/SurveyFragment.java
@@ -15,12 +15,10 @@ import android.view.ViewGroup;
  */
 public class SurveyFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -36,7 +34,6 @@ public class SurveyFragment extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment SurveyFragment.
      */
-    // TODO: Rename and change types and number of parameters
     public static SurveyFragment newInstance(String param1, String param2) {
         SurveyFragment fragment = new SurveyFragment();
         Bundle args = new Bundle();
@@ -59,6 +56,8 @@ public class SurveyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_survey, container, false);
+        View view = inflater.inflate(R.layout.fragment_survey, container, false);
+        //TODO - add image slideshow of possible pet results??
+        return view;
     }
 }
