f1df59cf20602687383ec8d6e87aa5b2c5594292
Ashley McCallum <w0765596@myscc.ca>
2021-11-06 23:02:01 -0400

    #14 Complete Resource class

--- app/src/main/java/com/ashleymccallum/thebarkinglot/ListView/Resource.java
+++ app/src/main/java/com/ashleymccallum/thebarkinglot/ListView/Resource.java
@@ -1,4 +1,27 @@
 package com.ashleymccallum.thebarkinglot.ListView;
 
 public class Resource {
+    private String item;
+    private int image;
+
+    public Resource(String item, int image) {
+        this.item = item;
+        this.image = image;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
 }
