73f8ed76051ef9951e7b1d2b4c1b684441e4417e
Ashley McCallum <w0765596@myscc.ca>
2021-11-13 20:23:33 -0500

    #20 Add Result fragment
    * Result fragment will display results to the screen

--- app/src/main/java/com/ashleymccallum/thebarkinglot/QuizResultsFragment.java
+++ app/src/main/java/com/ashleymccallum/thebarkinglot/QuizResultsFragment.java
@@ -3,28 +3,28 @@ package com.ashleymccallum.thebarkinglot;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link PetSurveyFragment#newInstance} factory method to
+ * Use the {@link QuizResultsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PetSurveyFragment extends Fragment {
+public class QuizResultsFragment extends Fragment {
 
+    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public PetSurveyFragment() {
+    public QuizResultsFragment() {
         // Required empty public constructor
     }
 
@@ -34,10 +34,11 @@ public class PetSurveyFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment PetSurveyFragment.
+     * @return A new instance of fragment QuizResultsFragment.
      */
-    public static PetSurveyFragment newInstance(String param1, String param2) {
-        PetSurveyFragment fragment = new PetSurveyFragment();
+    // TODO: Rename and change types and number of parameters
+    public static QuizResultsFragment newInstance(String param1, String param2) {
+        QuizResultsFragment fragment = new QuizResultsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -58,14 +59,6 @@ public class PetSurveyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_pet_survey, container, false);
-        Button submitButton = view.findViewById(R.id.submitButton);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.action_nav_pet_survey_to_nav_quiz_results);
-            }
-        });
-        return view;
+        return inflater.inflate(R.layout.fragment_quiz_results, container, false);
     }
 }
